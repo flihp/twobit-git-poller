e0fc8348af875d0689295188a8196b62cb1daf03
Philip Tricca <flihp@twobit.us>
2015-12-17 17:49:51 -0800

    gitpoller: Use python logger.

--- src/twobit_gitpoller/gitpoller.py
+++ src/twobit_gitpoller/gitpoller.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 import os, subprocess
-from twisted.python import log
+import logging as log
 from twobit_gitpoller import IPoll
 
 class GitPoller(IPoll):
@@ -10,6 +10,8 @@ class GitPoller(IPoll):
     """
 
     def __init__(self, repo_url = None, basedir = '/tmp', hook=None):
+        log.debug("GitPoller constructor: repo_url={0}, basedir={1}, hook={2}"
+                  .format(repo_url, basedir, hook))
         self.repo_url = repo_url
         self.basedir = basedir
         self.repo_name = self.repo_url.split('/')[-1]
@@ -24,38 +26,34 @@ class GitPoller(IPoll):
         """ Get list of strings representing branches for git repo
         """
         try:
-            print('git --git-dir={0} branch'.format(self.repo_path))
-            return subprocess.check_output(
-                       ['git', '--git-dir=' + self.repo_path, 'branch']
-                   ).decode('utf-8').replace('*', '').split()
+            cmd = ['git', '--git-dir={0}'.format(self.repo_path), 'branch']
+            log.info('executing cmd: {0}'.format(cmd))
+            return subprocess.check_output(cmd).decode('utf-8').replace('*', '').split()
         except subprocess.CalledProcessError, e:
-            print(e)
+            log.critical(e)
             return []
 
     def _get_symname(self, branch):
         """ Translate a branch name to a string holding full symbolic name
         """
         try:
-            print('git --git-dir={0} rev-parse --symbolic-full-name {1}'.format(self.repo_path, branch))
-            return subprocess.check_output(
-                       ['git', '--git-dir=' + self.repo_path, 'rev-parse',
-                        '--symbolic-full-name', branch]
-                   ).decode('utf-8').strip()
+            cmd = ['git', '--git-dir={0}'.format(self.repo_path), 'rev-parse',
+                   '--symbolic-full-name={0}'.format(branch)]
+            log.info('executing cmd: {0}'.format(cmd))
+            return subprocess.check_output(cmd).decode('utf-8').strip()
         except subprocess.CalledProcessError, e:
-            print(e)
+            log.critical(e)
             return None
 
     def _get_hash(self, name):
         """ Get the object hash for named object as string
         """
         try:
-            print('git --git-dir={0} rev-parse {1}'.format(self.repo_path, name))
-            return subprocess.check_output(
-                       ['git', '--git-dir=' + self.repo_path, 'rev-parse',
-                        name]
-                   ).decode('utf-8').strip()
+            cmd = ['git', '--git-dir={0}'.format(self.repo_path), 'rev-parse', name]
+            log.info('executing cmd: {0}'.format(cmd))
+            return subprocess.check_output(cmd).decode('utf-8').strip()
         except subprocess.CalledProcessError, e:
-            print(e)
+            log.critical(e)
             return None
 
     def poll(self):
@@ -66,14 +64,16 @@ class GitPoller(IPoll):
         locally it's fetched, otherwised a bare clone is made.
         """
         # sanity check config and directory structure
-        print('poller {0} polling for repo {1}'.format(id(self), self.repo_name));
+        log.info('poller {0} polling for repo {1}'
+                 .format(id(self), self.repo_name));
         if self.repo_url is None:
-            print('GitPoller: no repo url set, nothing to poll.')
+            log.info('GitPoller: no repo url set, nothing to poll.')
             return
         if not os.path.lexists(self.basedir):
             os.mkdir(self.basedir)
         elif not os.path.isdir(self.basedir):
-            print('GitPoller: {0} exists but is not a directory. Aborting'.format(self.basedir))
+            log.error('GitPoller: {0} exists but is not a directory. Aborting'
+                      .format(self.basedir))
         hook_data = []
         if (self.hook is not None and
             self.hook.script is not None and
@@ -84,32 +84,36 @@ class GitPoller(IPoll):
                 # for each branch create tuple of (symname, hash)
                 # & add to list
                 if branch is None or not branch:
-                    print('branch is None or empty')
+                    log.warning('branch is None or empty')
                     break
                 symname = self._get_symname(branch)
                 if symname is None or not symname:
-                    print('symname for branch "{0}" is None or empty'.format(branch))
+                    log.warning('symname for branch "{0}" is None or empty'
+                                    .format(branch))
                     break
                 hook_data.append((symname, self._get_hash(symname)))
-        print('for repo {0} at URL {1}: '.format(self.repo_name, self.repo_url), end='')
+        log.info('for repo {0} at URL {1}: '
+                 .format(self.repo_name, self.repo_url))
         try:
             if not os.path.exists(self.repo_path):
-                print('cloning mirror into {0}'.format(self.repo_path))
+                log.info('cloning mirror into {0}'.format(self.repo_path))
                 subprocess.check_output(
                     ['git', 'clone', '--mirror', self.repo_url, self.repo_path],
                     stderr=subprocess.STDOUT
-                    )
-            elif os.path.isdir(self.repo_path):
-                print('updating existing mirror')
-                subprocess.check_output(
-                    ['git', '--git-dir=' + self.repo_path, 'remote', 'update'],
-                    stderr=subprocess.STDOUT
                 )
+            elif os.path.isdir(self.repo_path):
+                log.debug('updating existing mirror')
+                cmd = ['git', '--git-dir={0}'.format(self.repo_path),
+                       'remote', 'update']
+                log.info('executing command: {0}'.format(cmd))
+                subprocess.check_output(cmd, stderr=subprocess.STDOUT)
             else:
-               print('{0} exists but is not a directory, bad news'.format(self.repo_path))
-            print('success polling repo: {0} by object {1}'.format(self.repo_url, id(self)))
+               log.error('{0} exists but is not a directory, bad news'
+                         .format(self.repo_path))
+            log.debug('success polling repo: {0} by object {1}'
+                      .format(self.repo_url, id(self)))
         except subprocess.CalledProcessError, e:
-           print('{0}'.format(e))
+           log.critical(e)
         if (self.hook is not None and
             self.hook.script is not None and
             len(hook_data) > 0):
@@ -117,27 +121,25 @@ class GitPoller(IPoll):
             # at a time (I think).
             for project in self.hook.projects:
                 try:
-                    p = subprocess.Popen(
-                            [self.hook.script,
-                             '--master={0}:{1}'.format(self.hook.host,
-                                                       self.hook.port),
-                             '--username=' + self.hook.user,
-                             '--auth=' + self.hook.passwd,
-                             '--logfile=' + self.hook.logfile,
-                             '--verbose', '--verbose', '--verbose',
-                             '--project=' + project
-                            ],
-                            env={'GIT_DIR' : self.repo_path},
-                            stdin=subprocess.PIPE
-                        )
+                    cmd = [self.hook.script,
+                           '--master={0}:{1}'.format(self.hook.host,
+                                                     self.hook.port),
+                           '--username=' + self.hook.user,
+                           '--auth=' + self.hook.passwd,
+                           '--logfile=' + self.hook.logfile,
+                           '--verbose', '--verbose', '--verbose',
+                           '--project=' + project]
+                    env = {'GIT_DIR' : self.repo_path}
+                    log.info('executing hook script: {0} with environment {1}'
+                             .format(cmd, env))
+                    p = subprocess.Popen(cmd, env=env, stdin=subprocess.PIPE)
                     for hook_set in hook_data:
                         post = self._get_hash(hook_set[0])
                         if post is None or not post:
-                            print(' post object hash is None or empty')
+                            log.warning(' post object hash is None or empty')
                             break
-                        p.stdin.write(
-                            '{0} {1} {2}\n'.format(hook_set[1], post, hook_set[0])
-                        )
+                        p.stdin.write('{0} {1} {2}\n'
+                                      .format(hook_set[1], post, hook_set[0]))
                     p.stdin.close()
                 except OSError, e:
-                    log.msg('Failed to execute hook script: {0}'.format(e))
+                    log.error('Failed to execute hook script: {0}'.format(e))
