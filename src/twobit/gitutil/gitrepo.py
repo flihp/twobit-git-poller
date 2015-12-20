d7b3d866eb7fe85de4abe486ce6ad17d6521a244
Philip Tricca <flihp@twobit.us>
2015-12-19 22:22:50 -0800

    gitpoller: Cleanup and add method to make gitdir accessible.

--- src/twobit/gitutil/gitrepo.py
+++ src/twobit/gitutil/gitrepo.py
@@ -12,6 +12,9 @@ class GitRepo(object):
         log.debug("GitRepo constructor")
         self._gitdir = gitdir
 
+    def get_gitdir(self):
+        return self._gitdir
+
     def get_branches(self):
         """ Get list of branch names for git repo """
         log.debug("get_branches")
@@ -41,34 +44,35 @@ class GitRepo(object):
 class GitMirror(object):
     """ Class used to maintain a mirror of a remote git repo
     """
-    def __init__(self, remote, mirror):
+    def __init__(self, remote=None, repo=None):
         """ Constructor
 
         remote: the URL for the remote repo to mirror
         mirror: path to the local mirror
         """
         log.debug("GitMirror init")
+        self._repo = repo
         self._remote = remote
-        self._mirror = mirror
 
     def mirror(self):
         if self._remote is None:
-            raise GitError("Mirror called without remote set")
-        if self._mirror is None:
-            raise GitError("Mirror called without gitdir set")
-        log.debug("Mirroring {0} at {1}.".format(self._remote, self._mirror))
-        cmd = ['git', 'clone', '--mirror', self._remote, self._mirror]
+            raise GitError("Mirror called without a repo set")
+        if self._repo is None:
+            raise GitError("Mirror called without a GitRepo object set")
+        log.debug("Mirroring {0} at {1}.".format(self._remote, self._repo.get_gitdir()))
+        cmd = ['git', 'clone', '--mirror', self._remote,
+               self._repo.get_gitdir()]
         log.info("Executing command: {0}".format(cmd))
         subprocess.check_output(cmd, stderr=subprocess.STDOUT)
 
     def update(self):
         """ Update an existing repo
         """
+        if self._repo is None:
+            raise GitError("Cannot update without a remote to update.")
         log.debug("Updating an existing mirror {0} from remote {1}."
-                  .format(self._mirror, self._remote))
-        if self._mirror is None:
-            raise GitError("Cannot update without gitdir set.")
-        cmd = ['git', '--git-dir={0}'.format(self._mirror), 'remote',
-               'update']
+                  .format(self._repo.get_gitdir(), self._remote))
+        cmd = ['git', '--git-dir={0}'.format(self._repo.get_gitdir()),
+               'remote', 'update']
         log.info("Executing command: {0}".format(cmd))
         subprocess.check_output(cmd, stderr=subprocess.STDOUT)
