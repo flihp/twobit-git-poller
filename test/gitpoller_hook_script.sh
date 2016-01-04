880853f11852ec1e350ee53926b8e4831913d5ab
Philip Tricca <flihp@twobit.us>
2016-01-03 21:50:14 -0800

    Cause gitpoller_hook_script.sh to preserve the log file.
    This is a pretty bad hack but at least it won't overwrite previous
    log files now. This allows for a test to call this hook multiple
    times and receive a log message for each invocation.

--- test/gitpoller_hook_script.sh
+++ test/gitpoller_hook_script.sh
@@ -5,8 +5,8 @@ LOGFILE=gitpoller_hook_script.log
     echo "$0 invoked with paramters: $@"
     echo "  and environment:"
     printenv
-} > ${LOGFILE} 2>&1
+} >> ${LOGFILE} 2>&1
 
 while read DATA; do
     echo "${DATA}"
-done > ${LOGFILE} 2>&1
+done >> ${LOGFILE} 2>&1
