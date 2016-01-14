d045a66f033e7009879d80d5570b04288b98cc07
Philip Tricca <flihp@twobit.us>
2016-01-14 06:26:33 +0000

    twobit.gitutil: Remove nonexistent module.

--- src/twobit/gitutil/__init__.py
+++ src/twobit/gitutil/__init__.py
@@ -4,4 +4,3 @@ from gitpoller import GitPoller
 from gitpollerfactory import GitPollerFactory, GitPollerFactoryValueError
 from githuborg import GitHubOrg
 from githuborgrepopoller import GitHubOrgRepoPoller, GitHubOrgRepoPollerFactory
-from gitpollerservicefactory import GitPollerServiceFactory, GitPollerServiceFactoryValueError
